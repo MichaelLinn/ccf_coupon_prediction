a360776b15bc155ba7df1ea3526f8773a850fab3
MichaelLinn <linjiecong@vip.qq.com>
2016-10-09 19:31:46 +0800

    init

--- coupon_prediction/pretreat_data/pretreatment.py
+++ coupon_prediction/pretreat_data/pretreatment.py
@@ -4,7 +4,25 @@ Created on '2016/10/9' '13:59'
 
 @author: 'michael"  
 """
-
+"""
+Action: 0 点击， 1购买，2领取优惠券
+Coupon_id: null表示无优惠券消费，此时Discount_rate和Date_received字段无意义。
+       “fixed”表示该交易是限时低价活动。
+Discount_rate: x \in [0,1]代表折扣率；x:y表示满x减y；“fixed”表示低价限时优惠；
+Date_received: 领取优惠券日期
+Date: 消费日期：如果Date=null & Coupon_id != null，该记录表示领取优惠券但没有使用；
+                如果Date!=null & Coupon_id = null，则表示普通消费日期；
+                如果Date!=null & Coupon_id != null，则表示用优惠券消费日期；
+"""
+"""
+Coupon_id : null表示无优惠券消费，此时Discount_id和Date_received字段无意义
+Discount_rate : 优惠率 x \in [0,1]代表折扣率; x:y 表示满x减y。单位是元
+Distance : 	user经常活动的地点离该merchant的最近门店距离是x*500米（如果是连锁店，则取最近的一家门店），
+            x\in[0,10]；null表示无此信息，0表示低于500米，10表示大于5公里；
+Date_received : 领取优惠券日期
+Date : 消费日期：如果Date=null & Coupon_id != null，该记录表示领取优惠券但没有使用，即负样本；
+       如果Date!=null & Coupon_id = null，则表示普通消费日期；如果Date!=null & Coupon_id != null，则表示用优惠券消费日期，即正样本；
+"""
 import numpy as np
 import pandas as pd
 import datetime
