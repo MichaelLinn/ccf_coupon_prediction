3864f641549902fc16ec50b1b04f8f450d93699a
MichaelLinn <linjiecong@vip.qq.com>
2016-10-10 21:33:16 +0800

    pretreatment

---
+++ coupon_prediction/pretreat_data/adaBoost.py
@@ -0,0 +1,135 @@
+# -*- coding:utf-8 -*- 
+"""
+Created on '2016/9/9' '10:32'
+
+@author: 'michael"  
+
+
+this adaboost model based on the decision stump,
+the decision trump classifies the data using only one feature at one time
+"""
+
+import numpy as np
+
+class adaBoost:
+
+    def __init__(self):
+        self.dataMat = np.matrix([[1., 2.1,50], [2., 1.1,50], [1.3, 1.,100], [1., 1.,100], [2., 1.,50]])
+        self.classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+
+    def loadSimpData(self):
+        dataMat = np.matrix([[1.,2.1],[2.,1.1],[1.3,1.],[1.,1.],[2.,1.]])
+        classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+        return dataMat,classLabels
+
+
+
+    def stumpDecisionTree(self,dataMat,dimen, thresholdVal, thresholdIneq):
+        retArray = np.ones((np.shape(dataMat)[0],1))   # nunmber of the data(vector) in dataMatrix
+        if thresholdIneq == 'lt':                       # two kinds of the inequality type
+            retArray[dataMat[:,dimen] <= thresholdVal] = -1.0
+            # print(dataMat[:,dimen].T , thresholdIneq , thresholdVal)
+            # print((dataMat[:,dimen] <= thresholdVal).T)
+        else:
+            retArray[dataMat[:,dimen] > thresholdVal] = -1.0
+            # print(dataMat[:, dimen].T, " ", thresholdVal)
+            # print((dataMat[:, dimen] <= thresholdVal).T)
+        return retArray
+
+    def buildStump(self,dataArr,classLabels,D):  # D is a vector of the data's weight
+        dataMatrix = np.mat(dataArr)
+        labelMat = np.mat(classLabels).T
+        m,n = np.shape(dataMatrix)
+        numSteps = 10.0
+        bestStump = {}
+        bestClassEst = np.mat(np.zeros((m,1)))
+        minError = np.inf
+        for i in range(n):
+            rangeMin = dataMatrix[:,i].min()
+            rangeMax = dataMatrix[:,i].max()
+            stepSize = (rangeMax - rangeMin)/numSteps
+            for j in range(-1,int(numSteps) + 1):
+                for inequal in ['lt','gt']:
+                    thresholdVal = (rangeMin + float(j) * stepSize)
+                    predictedVals = self.stumpDecisionTree(dataMatrix,i,thresholdVal,inequal)
+                    # print("Predict value:" , predictedVals.T)
+                    errArr = np.mat(np.ones((m,1)))
+                    errArr[predictedVals == labelMat] = 0   # set 0 to the vector which is classified correctly
+                    # print(predictedVals.T," ",labelMat.T)
+                    weightedError = D.T * errArr
+                    # print("split: dim %d, threshold value %.2f ,threshold inequal: %s, the weighted error is %.3f" %(i,thresholdVal,inequal,weightedError))
+                    if weightedError < minError:
+                        minError = weightedError
+                        bestClassEst = predictedVals.copy()
+                        bestStump['dimension'] = i
+                        bestStump['inequal'] = inequal
+                        bestStump['threshold'] = thresholdVal
+        return bestStump,minError,bestClassEst
+
+    def adaBoostTrainDecisionStump(self,dataArr,classLabels,numInt=40):
+        weakDecisionStumpArr = []
+        m = np.shape(dataArr)[0]
+        weight = np.mat(np.ones((m,1))/m)     # init the weight of the data.Normally, we set the initial weight is 1/n
+        aggressionClassEst = np.mat(np.zeros((m,1)))
+        for i in range(numInt): # classEst == class estimation
+            bestStump,error,classEst = self.buildStump(dataArr,classLabels,weight) # D is a vector of the data's weight
+            # print("D: ",weight.T)
+            alpha = float(0.5 * np.log((1.0 - error)/max(error , 1e-16)))   # alpha is the weighted of the weak classifier
+            bestStump['alpha'] = alpha
+            weakDecisionStumpArr.append(bestStump)
+            exponent = np.multiply(-1* alpha * np.mat(classLabels).T , classEst) # calculte the exponent [- alpha * Y * Gm(X)]
+            print("classEst ：",classEst.T)
+            weight = np.multiply(weight,np.exp(exponent)) # update the weight of the data, w_m = e^[- alpha * Y * Gm(X)]
+            weight = weight/weight.sum()  # D.sum() == Z_m (Normalized Factor) which makes sure the D_(m+1) can be a probability distribution
+            # give every estimated class vector (the classified result of the weak classifier) a weight
+            aggressionClassEst += alpha*classEst
+            print("aggression classEst: ",aggressionClassEst.T)
+            # aggressionClassError = np.multiply(np.sign(aggressionClassEst) != np.mat(classLabels).T, np.ones((m,1)))
+            # errorRate = aggressionClassError.sum()/m
+            errorRate = (np.sign(aggressionClassEst) != np.mat(classLabels).T).sum()/m # calculate the error classification
+            # errorRate = np.dot((np.sign(aggressionClassEst) != np.mat(classLabels).T).T,np.ones((m,1)))/m
+            print("total error: ",errorRate,"\n")
+            if errorRate == 0:
+                break
+        return weakDecisionStumpArr
+
+    """
+    def classify(self,adaboostModel,arr_single):
+        classLabel = []
+        # n_row,n_col = arr_single.shape
+        for i in range(len(adaboostModel)):
+            inequal = adaboostModel[i]['inequal']
+            if inequal == 'lt':
+                if arr_single[adaboostModel[i]['dimension']] <= adaboostModel[i]['threshold']:
+                    classLabel.append(-1)
+                else:
+                    classLabel.append(1)
+            else:
+                if arr_single[adaboostModel[i]['dimension']] >= adaboostModel[i]['threshold']:
+                    classLabel.append(-1)
+                else:
+                    classLabel.append(1)
+            classLabel[i] = classLabel[i] * adaboostModel[i]['alpha']
+        predictValue = np.sign(sum(classLabel))
+        return predictValue
+    """
+
+    def adaClassify(self,data,adaBoostModel):
+        dataMat = np.mat(data)
+        aggClassEst = np.mat(np.zeros((data.shape[0],1)))
+        for i in range(len(adaBoostModel)):
+            classLabels = self.stumpDecisionTree(dataMat,adaBoostModel[i]['dimension'],adaBoostModel[i]['threshold'],adaBoostModel[i]['inequal'])
+            aggClassEst += classLabels*adaBoostModel[i]['alpha']
+
+
+def main():
+    ab = adaBoost()
+    weight = np.mat(np.ones((5,1))/5)
+    ab.buildStump(ab.dataMat,ab.classLabels,weight)
+    adaBoostTrees = ab.adaBoostTrainDecisionStump(ab.dataMat,ab.classLabels,9)
+    arr = np.array([1.3,1.0,50])
+    preditVal = ab.classify(adaBoostTrees,arr)
+    print("predictVal:" , preditVal)
+
+if __name__ == '__main__':
+    main()
--- coupon_prediction/pretreat_data/pretreatment.py
+++ coupon_prediction/pretreat_data/pretreatment.py
@@ -4,7 +4,7 @@ Created on '2016/10/9' '13:59'
 
 @author: 'michael"  
 
-
+#online_data
 Action: 0 点击， 1购买，2领取优惠券
 Coupon_id: null表示无优惠券消费，此时Discount_rate和Date_received字段无意义。
        “fixed”表示该交易是限时低价活动。
@@ -14,6 +14,7 @@ Date: 消费日期：如果Date=null & Coupon_id != null，该记录表示领取
                 如果Date!=null & Coupon_id = null，则表示普通消费日期；
                 如果Date!=null & Coupon_id != null，则表示用优惠券消费日期；
 
+#offline_data
 Coupon_id : null表示无优惠券消费，此时Discount_id和Date_received字段无意义
 Discount_rate : 优惠率 x \in [0,1]代表折扣率; x:y 表示满x减y。单位是元
 Distance : 	user经常活动的地点离该merchant的最近门店距离是x*500米（如果是连锁店，则取最近的一家门店），
@@ -27,10 +28,14 @@ import numpy as np
 import pandas as pd
 import datetime
 import pickle
+import fnmatch
 
 offline_filename = "../../data/ccf_offline_stage1_train.csv"
 online_filename = "../../data/ccf_online_stage1_train.csv"
 test_filename = "../../data/ccf_offline_stage1_test.csv"
+
+offline_train_data_pklfilename = "./offline_train_data.pkl"
+washed_labeled_data_pklfilename = "./washed_labeled_data.pkl"
 
 def load_offline_data(filename = offline_filename):
     """
@@ -55,7 +60,6 @@ def load_offline_data(filename = offline_filename):
     data_offline['Date_received'] = np.array(rdate_list)
     data_offline['Date'] = np.array(udate_list)
     pickle._dump(data_offline,open("offline_train_data.pkl","wb"))
-    print(data_offline)
     return data_offline
 
 def load_online_data(filename = online_filename):
@@ -69,6 +73,7 @@ def load_online_data(filename = online_filename):
     header = ['User_id','Merchant_id','Action','Coupon_id',
               'Discount_rate','Date_received','Date']
     data_online = pd.read_csv(filename,names = header)
+
     return data_online
 
 def load_test_data(filename = test_filename):
@@ -82,13 +87,16 @@ def load_test_data(filename = test_filename):
     test_data = pd.read_csv(filename,names = header)
     return test_data
 
-def count_coupon_day(Date_received,Date_used):
+def count_coupon_day(pklfiname = offline_train_data_pklfilename):
+    offline_data = pickle.load(open(pklfiname,"rb"))
     day_beforeUsed = []
-    for i in range(len(Date_received)):
-        if Date_received[i] != "null" and Date_used[i] != "null":
-            day_beforeUsed.append(Date_used[i] - Date_received[i])
-    day_sorted = sorted(day_beforeUsed,reverse = True)
-    print(day_sorted)
+    for i in range(len(offline_data)):
+        if offline_data['Date_received'][i] != "null" and offline_data['Date'][i] != "null":
+            day_beforeUsed.append(offline_data['Date'][i] - offline_data['Date_received'][i])
+    day_sorted = np.array(sorted(day_beforeUsed,reverse = True))
+    pickle._dump(day_sorted,open("offline_coupon_using_day.pkl","wb"))
+    below15_days = len(day_sorted[day_sorted <= datetime.timedelta(15)])
+    print(below15_days)
 
 def random_test_toCSV(test_data):
     result_random = []
@@ -99,8 +107,163 @@ def random_test_toCSV(test_data):
     test_data.to_csv("test_result.csv",index = False , header = False ,
                      columns = ['User_id','Coupon_id','Date_received','result'])
 
+def label_offline_data(pklfilename = offline_train_data_pklfilename):
+    """
+    delete the irrelevant data and label the effective data
+    convert the distance_str into distance_int
+    # Date : 消费日期：如果Date=null & Coupon_id != null，该记录表示领取优惠券但没有使用，即负样本；
+             如果Date!=null & Coupon_id = null，则表示普通消费日期；如果Date!=null & Coupon_id != null，则表示用优惠券消费日期，即正样本；
+    :param pklfilename:
+    :return:
+    """
+    offline_data = pickle.load(open(pklfilename,"rb"))
+    labels = []
+    for i in range(len(offline_data)):
+        if offline_data['Coupon_id'][i] == "null":
+            labels.append(0)
+        else:
+            if offline_data['Date'][i] != "null":
+                labels.append(1)
+            else:
+                labels.append(-1)
+    offline_data['Label'] = np.array(labels)
+    pickle._dump(offline_data,open("labeled_offline_train_data.pkl","wb"))
+    washed_labeled_data = pd.DataFrame(offline_data[offline_data['Label'] != 0])
+    washed_labeled_data.to_csv("tem.csv",header=True,index=False)
+    washed_labeled_data = pd.read_csv("tem.csv")
+    print(washed_labeled_data)
+    # Warning: the index of dataframe cannot be modified,and the slices of the dataframe is not the copy of the dataframe
+    #          though we delete a row of the dataframe, we cannot change the indices
+
+    pickle._dump(washed_labeled_data,open("washed_labeled_data.pkl","wb"))
+
+
+# label_offline_data()
+# labeled_data = pickle.load(open("labeled_offline_train_data.pkl","rb"))
+# washed_labeled_data = labeled_data[labeled_data['Label'] != 0]
+# pickle._dump(washed_labeled_data,open("washed_labeled_data.pkl","wb"))
+
+"""
+# def dispose_null_distance(washed_labeled_data):
+# there is an ideal that using logistic model to predict the null value
+"""
+
+def convert_distance_int(pklfilename = washed_labeled_data_pklfilename):
+    washed_labeled_data = pickle.load(open(pklfilename,"rb"))
+    dis_list = []
+    for i in range(len(washed_labeled_data)):
+        if str(washed_labeled_data['Distance'][i]) != "null":
+            dis_list.append(int(washed_labeled_data['Distance'][i]))
+        else:
+            dis_list.append(12)
+    washed_labeled_data['Distance'] = np.array(dis_list)
+    pickle._dump(washed_labeled_data,open(washed_labeled_data_pklfilename,"wb"))
+
+def convert_discount_num(pklfilename = washed_labeled_data_pklfilename):
+    """
+    conver the '*:*' into float number
+    create two feature: one is total money ,the other is discount money
+    for example '50:10',total money is 50 , discount money is 10
+    :param  pklfilename:
+    :return:
+    """
+    upper_limit = []
+    allowance = []
+    discount_rate = []
+    washed_data = pickle.load(open(pklfilename,"rb"))
+    for i in range(len(washed_data)):
+        if washed_data['Discount_rate'][i].find(':') != -1:
+            tem = washed_data['Discount_rate'][i].split(':')
+            upper_limit.append(int(tem[0]))
+            allowance.append(int(tem[1]))
+            discount_rate.append(int(tem[0])*1.0 / int(tem[1]))
+        else:
+            upper_limit.append(100)
+            rate = 1 - float( washed_data['Discount_rate'][i] )
+            allowance.append(int(100*1.0*rate))
+            discount_rate.append(float(washed_data['Discount_rate'][i]))
+    washed_data['Discount_rate'] = np.array(discount_rate)
+    washed_data['Upper_limit'] = np.array(upper_limit)
+    washed_data['Allowance'] = np.array(allowance)
+    print(washed_data)
+
+
+# the training features are "Discount_rate" , "Upper_limit" , "Allowance", "Distance"
+# the class set is {-1,1}
+# file "washed_labeled_data.pkl" is the after pretreatment data set, whose type is pandas.Dataframe
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# days_used_received = pickle.load(open("offline_coupon_using_day.pkl","rb"))
+# print(len(days_used_received) ," : " , len(days_used_received[days_used_received <= datetime.timedelta(15)]))
+
+# print(washed_labeled_data)
+
+# convert_discount_num()
+# washed_labeled_data = label_offline_data(washed_labeled_data_pklfilename)
+# convert_distance_int()
+
+# label_offline_data()
+# washed_labeled_data = pickle.load(open(washed_labeled_data_pklfilename, "rb"))
+# if washed_labeled_data['Distance'][0] != "null":
+# print(washed_labeled_data)
+# pd = pd.DataFrame(washed_labeled_data)
+# washed_labeled_data.to_csv("test.csv",index=False,header = False)
+# data = pd.read_csv("test.csv")
+
 # random_test_toCSV(load_test_data())
 # test_data = load_test_data()
 # pickle._dump(test_data,open("test_data.pkl","wb"))
 # tem_data = pickle.load(open("offline_train_data.pkl","rb"))
 
+"""
+washed_labeled_data = pickle.load(open(washed_labeled_data_pklfilename,"rb"))
+tem = []
+for i in range(len(washed_labeled_data)):
+    if washed_labeled_data['Distance'][i] == -1:
+        tem.append(12)
+    else:
+        tem.append(washed_labeled_data['Distance'][i])
+washed_labeled_data['Distance'] = np.array(tem)
+print(washed_labeled_data[washed_labeled_data['Distance'] > 10])
+pickle._dump(washed_labeled_data,open(washed_labeled_data_pklfilename,"wb"))
+"""
